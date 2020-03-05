29f1a7df7e1de758c14ef5519d6d080d709835fa
gotparticles <50121310+gotparticles@users.noreply.github.com>
2020-03-05 15:31:33 -0500

    Add files via upload

---
+++ move.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+from time import sleep
+ 
+GPIO.setmode(GPIO.BOARD)
+ 
+Motor1A = 17
+ 
+GPIO.setup(Motor1A,GPIO.OUT)
+
+print "Turning motor on"
+GPIO.output(Motor1A,GPIO.HIGH)
+ 
+sleep(2)
+
+GPIO.cleanup()
